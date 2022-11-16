b721d55763533d6ef30b70c4fcd3e75511435b60
chengbotao <chengbotao5221@163.com>
2022-11-16 10:56:12 +0800

    fix(custom): vue 单元测试

--- packages/vue3xy/src/components/button/button.test.ts
+++ packages/vue3xy/src/components/button/button.test.ts
@@ -1,26 +1,20 @@
-import { render, fireEvent, cleanup } from '@testing-library/vue'
+import { render, cleanup } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
 import Button from "./index";
 describe('Button Component', () => {
     // 应呈现正确的默认按钮
-    test("should render the correct default button", () => {
-        let count = 0;
-        const { getByText } = render(Button, {
+    test("should render the correct default button", async () => {
+        const { getByText, emitted, ...rest } = render(Button, {
             slots: {
                 default: "Default Button"
-            },
-            attrs: {
-                onclick: ()=>{
-                    count++
-                }
             }
         });
         const element = getByText("Default Button") as HTMLButtonElement;
         expect(element.tagName).toEqual('BUTTON');
         expect(element.disabled).toBeFalsy();
         expect(element.className).toBe('xy-button xy-button-default');
-        fireEvent.click(element);
-        expect(count).toBe(1);
-        cleanup();
+        await userEvent.click(element);
+        expect(emitted("click")).toBeTruthy()
     });
 
     // 应该根据不同的props渲染正确的组件
@@ -37,7 +31,6 @@ describe('Button Component', () => {
         });
         const element = getByText("Primary Button") as HTMLButtonElement;
         expect(element.className).toBe('xy-button xy-custom-btn xy-button-primary xy-button-lg');
-        cleanup();
     })
     
     // 当 btnType 等于 link 并提供 href 时，应该呈现一个 A 标签
@@ -54,12 +47,11 @@ describe('Button Component', () => {
         const element = getByText("Link Button") as HTMLAnchorElement;
         expect(element.tagName).toEqual('A');
         expect(element.className).toBe('xy-button xy-button-link');
-        cleanup();
     })
 
     // 应呈现禁用按钮当 disabled 设置为 true
-    test("should render disabled button when disabled set to true", () => {
-        const { getByText } = render(Button, {
+    test("should render disabled button when disabled set to true", async () => {
+        const { getByText, emitted } = render(Button, {
             slots: {
                 default: "Disabled Button"
             },
@@ -69,7 +61,8 @@ describe('Button Component', () => {
         });
         const element = getByText("Disabled Button") as HTMLButtonElement;
         expect(element.disabled).toBeTruthy();
-        cleanup();
+        await userEvent.click(element);
+        expect(emitted("click")).toBeFalsy()
     })
 
 });
